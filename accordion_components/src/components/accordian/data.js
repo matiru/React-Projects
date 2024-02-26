1a4e7d96ac4ca93d0cbb66faa5286e085a3e67f3
matiru <gemmatiru@gmail.com>
2024-02-26 16:11:54 +0300

    accordion components

---
+++ accordion_components/src/components/accordian/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        id : '1',
+        question : 'What is Love?',
+        answer: "Baby don't hurt me"
+    },
+    {
+        id : '2',
+        question : 'Baby dont hurt me?',
+        answer: "No more"
+    },
+    {
+        id : '3',
+        question : 'Ta ta ta.ta ta ta ta ta.ta ?',
+        answer: "Ta"
+    },
+    {
+        id : '4',
+        question : 'I dont know why youre not fair?',
+        answer: "I give you my love, but you dont care"
+    },
+    {
+        id : '5',
+        question : 'What is right , what is wrong?',
+        answer: "Give me a sign"
+    }
+];
+
+export default data;
